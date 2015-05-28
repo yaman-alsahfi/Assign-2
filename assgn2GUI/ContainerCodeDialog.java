c81c527cd97aa9204cad43dd0ac8c3ffdb8a4a3b
Fatimah-Alraffaa <fattoo4@hotmail.com>
2015-05-28 18:55:05 +1000

    fixed the bugs

--- assgn2GUI/ContainerCodeDialog.java
+++ assgn2GUI/ContainerCodeDialog.java
@@ -3,8 +3,6 @@ package asgn2GUI;
 import java.awt.Color;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
-// import java.awt.event.ActionEvent;
-// import java.awt.event.ActionListener;
 
 
 
